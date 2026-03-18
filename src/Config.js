c2aa068592e3c2887359c183db59307f0487b43e
Martin Tsvetanov <martigiant@gmail.com>
2026-03-18 20:12:05 +0100

    Revert commit

--- src/Config.js
+++ src/Config.js
@@ -1,5 +1,9 @@
 const Config = {
+<<<<<<< HEAD
     BASE_URL: "http://localhost:3030/"
+=======
+    BASE_URL: 'https://tire-shop-api.herokuapp.com/'
+>>>>>>> parent of f9b0ba4 (return back to old url)
 };
 
 export default Config;
